caba60054ffe43651124f39a1c6d262e85a39f06
ronaldramosalvax-dotcom <ronaldramosalvax@gmail.com>
2026-06-01 10:42:27 -0500

    Update chat.js

--- chat.js
+++ chat.js
@@ -1,5 +1,5 @@
-import { initializeApp } from "https://gstatic.com";
-import { getDatabase, ref, push, onChildAdded, remove, orderByKey, limitToLast, query } from "https://gstatic.com";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.14.0/firebase-app.js";
+import { getDatabase, ref, push, onChildAdded, remove, orderByKey, limitToLast, query } from "https://www.gstatic.com/firebasejs/12.14.0/firebase-database.js";
 
 
 
